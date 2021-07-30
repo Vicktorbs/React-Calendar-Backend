f169662a948f19d5cb922cb0adda75a6a22e1e63
Victor Bolaños Solís <viktorb.132@gmail.com>
2021-07-29 19:14:35 -0500

    create user in db

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,36 @@
 const { response } = require('express');
 const { validationResult } = require('express-validator');
+const User = require('../models/User');
 
-const createUser = (req, res = response) => {
-    const { name, email, password } = req.body;
+const createUser = async(req, res = response) => {
+    const { email, password } = req.body;
 
-    res.status(201).json({
-        ok: true,
-        msg: 'Registro',
-        name,
-        email,
-        password
-    })
+    try {
+        let user = await User.findOne({ email });
+
+        if (user) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'The eamil is already used'
+            })
+        }
+        
+        user = new User(req.body);
+    
+        await user.save()
+    
+        res.status(201).json({
+            ok: true,
+            uid: user.id,
+            name: user.name
+        })
+    } catch (error) {
+        // console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Authentication problems, talk with the site admin'
+        })
+    }
 }
 
 const loginUser = (req, res) => {
